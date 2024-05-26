085e964156be488e3044eb01d213e7ed624f6eec
Sajjad Rahimi <sajjad021.5555@gmail.com>
2024-05-26 13:02:34 +0330

    create data for accordion create AccordionItem and state for open accordion and render it in Accordion componrnt

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,91 +1,48 @@
 import { useState } from "react";
-import { ChevronDownIcon } from "@heroicons/react/24/outline";
+
 const data = [
   {
     id: 1,
     title: "Accordion One",
-    text: "  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus quae fuga totam recusandae voluptate minus id aliquam possimus minima reprehenderit inventore fugiat voluptatem sequi, maiores accusantium animi doloribus similique tempore.",
+    text: "Lorem ipsum dolor, Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti alias porro aperiam quo at velit dicta voluptatum quas doloremque nihil, necessitatibus fugit iure, libero, cumque praesentium odio consectetur vero dignissimos!",
   },
   {
     id: 2,
     title: "Accordion Two",
-    text: "  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus quae fuga totam recusandae voluptate minus id aliquam possimus minima reprehenderit inventore fugiat voluptatem sequi, maiores accusantium animi doloribus similique tempore.",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti alias porro aperiam quo at velit dicta voluptatum quas doloremque nihil, necessitatibus fugit iure, libero, cumque praesentium odio consectetur vero dignissimos!",
   },
   {
     id: 3,
     title: "Accordion Three",
-    text: "  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus quae fuga totam recusandae voluptate minus id aliquam possimus minima reprehenderit inventore fugiat voluptatem sequi, maiores accusantium animi doloribus similique tempore.",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti alias porro aperiam quo at velit dicta voluptatum quas doloremque nihil, necessitatibus fugit iure, libero, cumque praesentium odio consectetur vero dignissimos!",
   },
 ];
 
 function Accordion() {
-  const [open, setOpen] = useState(null); // 1,2,3...
-
-  const handleOpen = (id) => {
-    setOpen(id === open ? null : id);
-  };
-
   return (
     <div className="accordion">
       {data.map((item) => (
-        <AccordionItem
-          key={item.id}
-          id={item.id}
-          title={item.title}
-          onOpen={handleOpen}
-          // setOpen={setOpen}
-          open={open}
-        >
-          {item.text}
-        </AccordionItem>
+        <AccordionItem key={item.id} item={item} />
       ))}
-      <AccordionItem
-        id={4}
-        title="Another Accordion"
-        onOpen={handleOpen}
-        // setOpen={setOpen}
-        open={open}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae
-          blanditiis maiores fugit reprehenderit accusamus vitae voluptate
-          perferendis labore debitis. Quae tempora cumque laudantium hic
-          consequatur natus qui deleniti id commodi?
-        </p>
-        <ul>
-          <li>one</li>
-          <li>two</li>
-        </ul>
-        <a href="#">Dummy Link !</a>
-      </AccordionItem>
     </div>
   );
 }
 
 export default Accordion;
 
-function AccordionItem({ id, title, setOpen, open, onOpen, children }) {
-  const isOpen = id === open;
+function AccordionItem({ item }) {
+  // state
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className={`accordion-item ${isOpen ? "accordion__expanded" : ""}`}>
       <div
         className="accordion-item__header"
-        // onClick={() => setOpen(item.id)}
-        onClick={() => onOpen(id)}
+        onClick={() => setIsOpen((is) => !is)}
       >
-        <div>{title}</div>
-        <ChevronDownIcon
-          className="accordion-item__chevron"
-          // style={{
-          //   width: "1.2rem",
-          //   transition: "all 0.2s ease-out",
-          //   rotate: isOpen ? "180deg" : "0deg",
-          // }}
-        />
+        {item.title}
       </div>
-      <div className="accordion-item__content">{children}</div>
-      {/* {isOpen && <div className="accordion-item__content">{item.text}</div>} */}
+      <div className="accordion-item__content">{item.text}</div>
     </div>
   );
 }
